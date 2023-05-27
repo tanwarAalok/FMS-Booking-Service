aa5a52d36c2846f131aaf7854c2652f1edaa6592
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-05-27 12:54:15 +0530

    added mvc boilerplate for booking

---
+++ src/repositories/booking-repository.js
@@ -0,0 +1,10 @@
+const CrudRepository = require("./crud-repository");
+const { Booking } = require("../models");
+
+class BookingRepository extends CrudRepository {
+  constructor() {
+    super(Booking);
+  }
+}
+
+module.exports = BookingRepository;

3143dd08cf1eaa121917716f878b086a7b0f1972
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-02 18:11:30 +0530

    made payment api idempotent

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,8 @@ const { BookingService } = require("../services");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
 const { StatusCodes } = require("http-status-codes");
 
+const inMemoryDB = {}
+
 async function createBooking(req, res) {
   try {
     const response = await BookingService.createBooking({
@@ -19,11 +21,21 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
+    const idempotencyKey = req.headers['x-idempotency-key'];
+    if (!idempotencyKey) {
+      return res.status(StatusCodes.BAD_REQUEST).json({message: "idempotencyKey not found"});
+    }
+    if (inMemoryDB[idempotencyKey]) {
+      return res.status(StatusCodes.BAD_REQUEST).json({message: "Cannot retry on successful request"});
+    }
+
     const response = await BookingService.makePayment({
       bookingId: req.body.bookingId,
       totalCost: req.body.totalCost,
       userId: req.body.userId
     });
+    inMemoryDB[idempotencyKey] = idempotencyKey;
+
     SuccessResponse.data = response;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
